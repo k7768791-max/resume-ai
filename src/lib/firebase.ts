ac85a189562e18bfc92367a4d0f4ae0129f1f450
Kittu <k7768791@gmail.com>
2025-09-08 05:40:51 +0000

    I HAVE MNAUVALLY CRETED THE APPLIACTION HERE IS THE KEYS:const firebaseC

---
+++ src/lib/firebase.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps, getApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB6jAqtsKzEoWkdNRUkaP_uiFwL5dvY3II",
+  authDomain: "resumeai-aey6u.firebaseapp.com",
+  projectId: "resumeai-aey6u",
+  storageBucket: "resumeai-aey6u.appspot.com",
+  messagingSenderId: "98852257242",
+  appId: "1:98852257242:web:cc29b9fb66f78d764794e3"
+};
+
+
+// Initialize Firebase
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const storage = getStorage(app);
+
+export { app, auth, storage };
